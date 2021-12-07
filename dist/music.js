85c73a468e31db5c29b0a6bf4fe9d0d7a8bb02aa
Natsumeaiovo <137641553@qq.com>
2021-12-07 23:55:00 +0800

    Site updated: 2021-12-07 23:55:00

--- dist/music.js
+++ dist/music.js
@@ -9,9 +9,9 @@ const ap = new APlayer({
       url: 'https://kusanagi.oss-cn-beijing.aliyuncs.com/music/%E3%81%93%E3%81%AE%E6%AB%BB%E3%83%8E%E8%A9%A9%E3%81%AE%E4%B8%8B.mp3',
       cover: 'https://kusanagi.oss-cn-beijing.aliyuncs.com/cover/%E6%A8%B1%E4%B9%8B%E8%AF%97OST.jpg',
     },
-/*     {
-      name: '',
-      artist: '',
+    {
+      name: 'ジムノペディ',
+      artist: '松本文紀',
       url: '',
       cover: '',
     },
@@ -20,6 +20,6 @@ const ap = new APlayer({
       artist: '',
       url: '',
       cover: '',
-    } */
+    }
   ]
 });
